463987ad2c1aa60a713d750c376ceb1bc3900ed1
Jeff Conn <jeff.conn121@gmail.com>
2022-05-02 11:37:16 -0400

    added google maps link to next game

--- src/components/NextGame.js
+++ src/components/NextGame.js
@@ -15,7 +15,9 @@ function NextGame() {
         <div className='game-details-container'>
             <hr />
             <h1 className='game-details-title'>{title}
-            <p className='game-details-date'>Date and Time: {apiGameData[0].description}</p></h1>
+            <p className='game-details-date'>Date and Time: {apiGameData[0].description}</p>
+            <a className='game-location' href='https://www.google.com/maps/place/745+Wolf+Creek+Rd,+Pickens,+SC+29671/@34.9188306,-82.6311605,17z/data=!3m1!4b1!4m5!3m4!1s0x88584be9996cf69f:0x35d7680cb3a51dbb!8m2!3d34.9188306!4d-82.6289718'>Get Directions</a>
+            </h1>
             <hr />
             <img  className="game-details-picture" src={apiGameData[0].imgUrl} alt="" />
         </div>
